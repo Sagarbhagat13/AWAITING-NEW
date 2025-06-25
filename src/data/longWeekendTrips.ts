a88379cc5e38ad3ef880ae9e4187254dcc57e166
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-06-25 16:54:13 +0530

    Update longWeekendTrips.ts

--- src/data/longWeekendTrips.ts
+++ src/data/longWeekendTrips.ts
@@ -13,7 +13,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     location: 'Himachal Pradesh',
     image: 'https://images.unsplash.com/photo-1697984286841-5cedb7885109?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 19999,
-    duration: '3D/2N',
+    duration: '8D/7N',
     longWeekend: true,
     rating: 4.7,
     reviews: 120
@@ -35,7 +35,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     location: 'Ladakh',
     image: 'https://images.unsplash.com/photo-1566323124805-757e5c41d37c?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 18999,
-    duration: '3D/2N',
+    duration: '6D/5N',
     longWeekend: true,
     rating: 4.6,
     reviews: 98
@@ -46,7 +46,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     location: 'Kerala',
     image: 'https://images.unsplash.com/photo-1593693411515-c20261bcad6e?q=80&w=2338&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 14900,
-    duration: '4D/3N',
+    duration: '8D/7N',
     longWeekend: true,
     rating: 4.9,
     reviews: 135
@@ -57,7 +57,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     location: 'Rajasthan',
     image: 'https://images.unsplash.com/photo-1587295656906-b06dca8f2340?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 15900,
-    duration: '5D/4N',
+    duration: '8D/7N',
     longWeekend: true,
     rating: 4.8,
     reviews: 142
@@ -69,7 +69,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     location: 'Ladakh',
     image: 'https://images.unsplash.com/photo-1738494777740-21b07e9da9a2?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 32999,
-    duration: '5D/4N',
+    duration: '12D/11N',
     longWeekend: true,
     rating: 4.8,
     reviews: 142
