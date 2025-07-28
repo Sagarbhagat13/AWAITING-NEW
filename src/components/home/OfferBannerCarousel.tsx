fd36ed7c8beccf9966eb9d9addd95b855aa9394a
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-07-28 13:39:04 +0530

    Update OfferBannerCarousel.tsx

--- src/components/home/OfferBannerCarousel.tsx
+++ src/components/home/OfferBannerCarousel.tsx
@@ -20,7 +20,7 @@ const offerBanners: OfferBanner[] = [
     id: '1',
     title: '',
     description: '',
-    desktopImage: '/images/Spiti Valley banner.png',
+    desktopImage: 'https://images.unsplash.com/photo-1649972904349-6e44c42644a7?auto=format&fit=crop&q=80&w=1200&h=400',
     mobileImage: 'https://plus.unsplash.com/premium_photo-1692386759833-3acf660742ad?q=80&w=2352&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&q=80&w=800&h=600',
     ctaText: 'Book Now',
     ctaLink: '#'
