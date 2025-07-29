e904f256707f06a7953e82c0a0f1a1aaad230be4
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-07-29 17:35:43 +0530

    Update longWeekendTrips.ts

--- src/data/longWeekendTrips.ts
+++ src/data/longWeekendTrips.ts
@@ -35,7 +35,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     location: 'Ladakh',
     image: 'https://images.unsplash.com/photo-1566323124805-757e5c41d37c?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 18999,
-    duration: '6D/5N',
+    duration: '7D/6N',
     longWeekend: true,
     rating: 4.6,
     reviews: 98
