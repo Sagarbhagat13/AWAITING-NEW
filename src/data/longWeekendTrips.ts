0855318a82ed434d9f2709393847ad9df42fbf3f
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-07-29 16:33:01 +0530

    Update longWeekendTrips.ts

--- src/data/longWeekendTrips.ts
+++ src/data/longWeekendTrips.ts
@@ -24,7 +24,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     location: 'Himachal Pradesh',
     image: 'https://images.unsplash.com/photo-1653844573020-71f77a0ccb8c?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 31999,
-    duration: '3D/2N',
+    duration: '8D/7N',
     longWeekend: true,
     rating: 4.8,
     reviews: 156
