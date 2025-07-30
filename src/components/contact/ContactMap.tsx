3298b1b825640a0df7ef08502a04fcf973145898
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-07-30 22:54:24 +0530

    Update ContactMap.tsx

--- src/components/contact/ContactMap.tsx
+++ src/components/contact/ContactMap.tsx
@@ -6,8 +6,7 @@ const ContactMap = () => {
   return (
     <div className="space-y-8">
       <div className="h-[300px] rounded-2xl overflow-hidden contact-map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4653.493173574879!2d72.91828900000002!3d19.1112301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cf3458f47971%3A0xd7b2f1e50522fcdf!2sAwaiting%20Adventures%20Travel%20Experiences!5e1!3m2!1sen!2sin!4v1753890308205!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        {/* <iframe 
+        <iframe 
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4653.493173574879!2d72.91828900000002!3d19.1112301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cf3458f47971%3A0xd7b2f1e50522fcdf!2sAwaiting%20Adventures%20Travel%20Experiences!5e1!3m2!1sen!2sin!4v1753890308205!5m2!1sen!2sin" 
           width="100%" 
           height="100%" 
@@ -15,7 +14,7 @@ const ContactMap = () => {
           allowFullScreen 
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
-        ></iframe> */}
+        ></iframe>
       </div>
       
       <div className="bg-white p-6 rounded-2xl shadow-sm space-y-6">
