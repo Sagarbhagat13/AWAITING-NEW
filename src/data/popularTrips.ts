b16b5862d9723168e33bd932db641a8f46b8b82c
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-07-30 17:47:01 +0530

    Update popularTrips.ts

--- src/data/popularTrips.ts
+++ src/data/popularTrips.ts
@@ -63,7 +63,7 @@ export const popularTrips: Trip[] = [
     image: 'https://images.unsplash.com/photo-1653844573020-71f77a0ccb8c?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     rating: 4.7,
     reviews: 176,
-    independence  : true,
+    independence : true,
     
   },
   {
