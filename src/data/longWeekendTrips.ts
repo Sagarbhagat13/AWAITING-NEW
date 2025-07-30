7e5a6972cd48520a546da7a74baf124b355aa3d5
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-07-30 14:43:29 +0530

    ss
    s

--- src/data/longWeekendTrips.ts
+++ src/data/longWeekendTrips.ts
@@ -14,7 +14,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     image: 'https://images.unsplash.com/photo-1697984286841-5cedb7885109?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 17999,
     duration: '8D/7N',
-    longWeekend: true,
+  
     rating: 4.7,
     reviews: 120,
     independence: true
