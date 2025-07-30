bfafe7becfd576d625acac96dab0143a066a88c0
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-07-30 14:41:32 +0530

    test

--- src/data/longWeekendTrips.ts
+++ src/data/longWeekendTrips.ts
@@ -16,7 +16,8 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     duration: '8D/7N',
     longWeekend: true,
     rating: 4.7,
-    reviews: 120
+    reviews: 120,
+    independence: true
   },
   {
     id: 'lw2',
@@ -25,6 +26,7 @@ export const longWeekendTrips: LongWeekendTrip[] = [
     image: 'https://images.unsplash.com/photo-1653844573020-71f77a0ccb8c?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     price: 31999,
     duration: '8D/7N',
+    independence: true,
     longWeekend: true,
     rating: 4.8,
     reviews: 156
--- src/data/popularTrips.ts
+++ src/data/popularTrips.ts
@@ -37,7 +37,7 @@ export const popularTrips: Trip[] = [
     image: 'https://images.unsplash.com/photo-1695956353120-54ce5e91632b?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     rating: 4.8,
     reviews: 245,
-    independence: true,
+    
   },
   {
     id: 'lw4',
