7b69440e5ca7ad51ffdd84b9521ec242b914258b
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-09-11 15:12:51 +0530

    Update RAJASTHAN JJJ XMAS.ts

--- src/data/itineraries/RAJASTHAN JJJ XMAS.ts
+++ src/data/itineraries/RAJASTHAN JJJ XMAS.ts
@@ -10,11 +10,11 @@ export const rjjjjxmas = {
   coverImage: 'https://images.unsplash.com/photo-1472396961693-142e6e269027?auto=format&fit=crop&w=1200&q=80',
   rating: 4.7,
   reviews: 89,
-   images: [
-  'https://images.unsplash.com/photo-1603262110263-fb0112e7cc33?auto=format&fit=crop&w=1200&q=80', // Hawa Mahal, Jaipur
-  'https://images.unsplash.com/photo-1603262110304-3fa55b46a35c?auto=format&fit=crop&w=1200&q=80', // Amber Fort, Jaipur
-  'https://images.unsplash.com/photo-1599661046289-7d64e47ceaa4?auto=format&fit=crop&w=1200&q=80', // City Palace, Udaipur
-  'https://images.unsplash.com/photo-1564507592333-c60657eea523?auto=format&fit=crop&w=1200&q=80'  // Sand dunes, Jaisalmer
+  images: [
+  'https://unsplash.com/photos/birds-flying-over-river-during-daytime-uADXI1v10us?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash', // Hawa Mahal, Jaipur
+  'https://images.unsplash.com/photo-1548013146-72479768bada?auto=format&fit=crop&w=1200&q=80', // Amer Fort, Jaipur
+  'https://images.unsplash.com/photo-1564507592333-c60657eea523?auto=format&fit=crop&w=1200&q=80', // Sand dunes, Jaisalmer
+  'https://images.unsplash.com/photo-1589739906089-3584f0f3a43b?auto=format&fit=crop&w=1200&q=80'  // City Palace, Udaipur
 ],
 
 
