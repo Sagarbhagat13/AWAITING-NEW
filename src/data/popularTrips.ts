90ef484af941f4f0b5d39856f52fa3d7ab481754
Sagarbhagat13 <87600386+Sagarbhagat13@users.noreply.github.com>
2025-09-11 16:23:47 +0530

    too many

--- src/data/popularTrips.ts
+++ src/data/popularTrips.ts
@@ -28,30 +28,71 @@ export interface Trip {
 
 export const popularTrips: Trip[] = [
   {
-    id: 'lw5',
-    title: 'Experience Rajasthan in monsoon',
-    location: 'Rajasthan',
-    price: 15900,
-    discount: 0,
-    duration: '8D/7N',
-    image: 'https://images.unsplash.com/photo-1695956353120-54ce5e91632b?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    rating: 4.8,
-    reviews: 245,
-    diwali: true,
-    
+    id: 'lw16',
+    title: 'Himachal with Dharamshala and manali',
+    location: 'Himachal Pradesh',
+    image: '/images/xmasnyhima2.png',
+    price: 19999,
+    duration: '7N/8D',
+    longWeekend: true,
+    rating: 5.0,
+    reviews: 167
   },
   {
-    id: 'lw4',
-    title: 'Kerala mansoon',
-    location: 'kerala',
-    price: 14900,
-    discount: 0,
-    duration: '8D/7N',
-    image: 'https://images.unsplash.com/photo-1629813538702-64c925934e19?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    id: 'lw12',
+    title: 'Meghalaya with Kaziranga: Wildlife, Waterfalls & Wonders',
+    location: 'Meghalaya & Assam',
+    image: 'https://images.unsplash.com/photo-1625826415766-001bd75aaf52?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWVnaGFsYXlhfGVufDB8fDB8fHww',
+    price: 23999,
+    duration: '6N/7D',
+    longWeekend: true,
     rating: 4.9,
-    reviews: 189,
-   diwali: true,
-    
+    reviews: 154
+  },
+  {
+    id: 'lw8',
+    title: 'Winter Spiti valley',
+    location: 'Himachal Pradesh',
+    image: '/images/xmasnyspiti1.png',
+    price: 19999,
+    duration: '5N/6D',
+    longWeekend: true,
+    rating: 4.8,
+    reviews: 189
+  },
+  ,
+  {
+    id: 'lw9',
+    title: 'Winter Rann of Kutch',
+    location: 'Gujarat',
+    image: '/images/xmasnyrann.png',
+    price: 7499,
+    duration: '1N/2D',
+    longWeekend: true,
+    rating: 4.9,
+    reviews: 156
+  },
+  {
+    id: 'lw11',
+    title: 'Experience Best Of Uttarakhand ',
+    location: 'Uttarakhand',
+    image: '/images/Uttarakhand.png',
+    price: 22499,
+    duration: '6N/7D',
+    longWeekend: true,
+    rating: 4.8,
+    reviews: 134
+  },
+  {
+    id: 'lw14',
+    title: 'Sikkim & Darjeeling Delight',
+    location: 'sikkim',
+    image: '/images/Sikkim.webp',
+    price: 21999,
+    duration: '5N/6D',
+    longWeekend: true,
+    rating: 4.8,
+    reviews: 203
   },
   {
     id: 'lw1',
@@ -89,5 +130,37 @@ export const popularTrips: Trip[] = [
     rating: 4.9,
     reviews: 267,
     diwali: true,
+  },  {
+    id: 'lw4',
+    title: 'Kerala Holiday Special',
+    location: 'Kerala',
+    image: '/images/xmasnykr.png',
+    price: 18000,
+    duration: '5N/6D',
+    longWeekend: true,
+    rating: 4.9,
+    reviews: 145
+  },
+  {
+    id: 'lw5',
+    title: 'Experience Rajasthan',
+    location: 'Rajasthan',
+    image: '/images/xmasnyrj.png',
+    price: 20900,
+    duration: '5N/6D',
+     longWeekend: true,
+    rating: 4.8,
+    reviews: 203
+  },
+  {
+    id: 'lw7',
+    title: 'Simpply Meghalaya',
+    location: 'Meghalaya',
+    image: '/images/xmasnymg.png',
+    price: 19999,
+    duration: '5N/6D',
+    longWeekend: true,
+    rating: 4.7,
+    reviews: 167
   },
 ];
